01c2af1ba0aab17ad6aedfecd061bafbc71f7c33
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-09-05 18:03:39 -0300

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,7 +19,8 @@ userRouter.post('/api/user/signup', async (req, res) => {
                 full_name: name,
                 phone_number: phone,
                 birthdate: birth,
-                cpf: cpf
+                cpf: cpf,
+                full_user_access: false
                 }
             }
         });
@@ -80,7 +81,28 @@ userRouter.get('/api/user/auth', protectRoute, async (req, res) => {
     }
 
     // Retorna as informações seguras do usuário, incluindo os metadados.
-    res.status(200).json({user: true})
+    res.status(200).json({ user: true})
+});
+
+userRouter.get('/api/user/full_access', protectRoute, async (req, res) => {
+    // Se o middleware 'protectRoute' passou, o usuário é válido.
+    const accessToken = req.cookies['sb-access-token'];
+
+    // Se não houver token, retorne um erro (embora o protectRoute já deva fazer isso).
+    if (!accessToken) {
+        return res.status(401).json({ error: 'Token de acesso não fornecido.' });
+    }
+
+    // Usamos o token para obter os detalhes do usuário do Supabase.
+    const { data: { user }, error } = await supabase.auth.getUser(accessToken);
+
+    if (error || !user) {
+        // Se houver um erro ou o usuário não for encontrado, retorna um erro.
+        return res.status(401).json({ error: 'Falha ao autenticar usuário. Token inválido ou expirado.' });
+    }
+
+    // Retorna as informações seguras do usuário, incluindo os metadados.
+    res.status(200).json({ full_user_access: user.user_metadata?.full_user_access ?? false });
 });
 
 userRouter.get('/api/user/data', protectRoute, async (req, res) => {
