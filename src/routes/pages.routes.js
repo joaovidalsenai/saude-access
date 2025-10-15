3a4976e51447f69af00b09c20b26ec3a1e49413f
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-10-15 19:06:40 -0300

    Update pages.routes.js

--- src/routes/pages.routes.js
+++ src/routes/pages.routes.js
@@ -428,12 +428,9 @@ pages.get('/api/hospitais/buscar', protect.entirely, async (req, res) => {
 
 pages.get('/cadastro/info', protect.partially, (req, res) => res.render('cadastroInfo'));
 
-<<<<<<< Updated upstream
 pages.get('/sobre-nos', protect.partially, (req, res) => res.render('sobrenos'));
-=======
 pages.get('/recuperar-senha', (req, res) => res.render('recuperarSenha'));
 
 pages.get('/redefinir-senha', (req, res) => res.render('redefinirSenha'));
->>>>>>> Stashed changes
 
 export default pages;
